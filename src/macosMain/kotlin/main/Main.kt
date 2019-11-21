9730446277f59069e5a372a8ef8602b5b963328b
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-11-21 07:19:47 +0300

    !!!!

--- src/macosMain/kotlin/main/Main.kt
+++ src/macosMain/kotlin/main/Main.kt
@@ -1,6 +1,8 @@
 package main
 
-import kotlinx.cli.*
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import kotlinx.io.*
 
 fun main (args: Array<String>) {
     args.forEach(::println)
